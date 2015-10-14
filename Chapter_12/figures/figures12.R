a5367c81522af6d3029efa233e09b14646c5885d
csortu <csaba.ortutay@gmail.com>
2015-10-14 11:26:26 +0300

    Adding code for figs 12.5 and 12.6

--- Chapter_12/figures/figures12.R
+++ Chapter_12/figures/figures12.R
@@ -63,3 +63,61 @@ legend("topleft",
        bty="n",
        lty=c(1,2))
 dev.off()
+
+## figure 12.5
+
+library(readxl)
+xls.file <- "240214_data.xlsx"
+ch450 <- read_excel(xls.file,sheet=1)
+ch570 <- read_excel(xls.file,sheet=2)
+samples <- read_excel(xls.file,sheet=3)
+ch450[,1] <- NULL
+ch570[,1] <- NULL
+samples[,1] <- NULL
+elisa.data <- data.frame(ch450=unlist(ch450),ch570=unlist(ch570),sample=unlist(samples))
+elisa.data$od <- elisa.data$ch450 - elisa.data$ch570
+elisa.standard <- data.frame(lab=elisa.data$sample[grepl("IL-2",elisa.data$sample)],od=elisa.data$od[grepl("IL-2",elisa.data$sample)])
+library(stringr)
+conc.pat <- "(\\d+\\.?\\d+)"
+elisa.standard$conc <- as.numeric(str_extract(elisa.standard$lab,conc.pat))
+library(drc)
+elisa.st.4pl <- drm(formula = conc ~ od, data = elisa.standard, fct = LL.4())
+elisa.data$conc.4pl <- predict(elisa.st.4pl,data.frame(od=elisa.data$od))
+
+elisa.data$animal[grep("IL-2",elisa.data$sample,invert=T)] <- unlist(strsplit(as.character(elisa.data$sample[grep("IL-2", elisa.data$sample,invert=T)]), " "))[seq(from=1, by=4, length.out=80)]
+elisa.data$dilution[grep("IL-2",elisa.data$sample,invert=T)] <- unlist(strsplit(as.character(elisa.data$sample[grep("IL-2", elisa.data$sample,invert=T)]), " "))[seq(from=2, by=4, length.out=80)]
+elisa.data$treatment[grep("IL-2",elisa.data$sample,invert=T)] <- unlist(strsplit(as.character(elisa.data$sample[grep("IL-2", elisa.data$sample,invert=T)]), " "))[seq(from=3, by=4, length.out=80)]
+elisa.data$time[grep("IL-2",elisa.data$sample,invert=T)] <- unlist(strsplit(as.character(elisa.data$sample[grep("IL-2", elisa.data$sample,invert=T)]), " "))[seq(from=4, by=4, length.out=80)]
+elisa.data$genotype <- substr(elisa.data$animal,1,2)
+
+measured.data <- elisa.data[elisa.data$time=="4h"&!is.na(elisa.data$animal),c(10:6,5)]
+measured.data <- rbind(measured.data,elisa.data[elisa.data$time=="24h"&elisa.data$dilution=="1/100"&!is.na(elisa.data$animal),c(10:6,5)])
+measured.data <- rbind(measured.data,elisa.data[elisa.data$time=="48h"&elisa.data$dilution=="1/1000"&!is.na(elisa.data$animal),c(10:6,5)])
+
+measured.data$undil.conc <- measured.data$conc.4pl
+measured.data$undil.conc[measured.data$dilution=="1/100"] <- measured.data$undil.conc[measured.data$dilution=="1/100"] * 100
+measured.data$undil.conc[measured.data$dilution=="1/1000"] <- measured.data$undil.conc[measured.data$dilution=="1/1000"] * 1000
+
+measured.data$time <- as.factor(measured.data$time)
+measured.data$time <- relevel(measured.data$time,"4h")
+
+tiff("figure12_5.tif",width=5,height=5,units="in",res=1200)
+boxplot(undil.conc ~ time,data=measured.data,log="y",
+        main="IL-2 production of T-cells\ntime series",
+        ylab="IL-2 [pg/ml]")
+dev.off()
+
+## figure 12.6
+
+tiff("figure12_6.tif",width=5,height=5,units="in",res=1200)
+par(xaxt="n")
+boxplot(undil.conc ~ time + genotype, 
+        data=measured.data[measured.data$treatment=="Tr1",], 
+        log="y", 
+        main="IL-2 production of T-cells in FURIN WT/KO mice", 
+        ylab="IL-2 [pg/ml]")
+lablist <- rep(c("4h","24h","48h"),2)
+text(1:6, 4, labels = lablist, srt = 0, pos = 1, xpd = TRUE)
+lablist <- rep(c("KO","WT"),each=3)
+text(1:6, 8, labels = lablist, srt = 0, pos = 1, xpd = TRUE)
+dev.off() 
